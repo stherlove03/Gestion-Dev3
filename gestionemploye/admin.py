317c2b697a2fc181fa236c877f78fda8094c837d
Esther Love Prisnie Desravines <prisnie11@gmail.com>
2024-04-06 08:15:45 -0400

    création d'un utilisateur super administrateur pour la gestion des autorisations.

--- gestionemploye/admin.py
+++ gestionemploye/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Employe, Departement
 # Register your models here.
+
+class affichageemploye(admin.ModelAdmin):
+    list_display = ['cin','nom','prenom','date_naissance', 'sexe', 'email', 'telephone', 'adresse','titre','departement', 'salaire', 'date_debut_travail']
+
+admin.site.register(Employe , affichageemploye)
+
+class affichagedepartement(admin.ModelAdmin):
+    list_display = ['code','nom','description']
+
+admin.site.register(Departement, affichagedepartement)
